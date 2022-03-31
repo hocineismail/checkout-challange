ffa963d0c0ff5acac83e04a8633e6ac79da9250f
hocineismail <hocineismail75@gmail.com>
2022-03-31 22:50:57 +0100

    Make some examples for checkout system

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,29 @@
-console.log("hello")
+import { orders__1, orders__2, orders__3 } from "../data/orders";
+import { IProduct } from "../typings/product";
+ 
+import { Checkout } from "./checkout"; 
+import { products } from "./products/products";
+import { GlobalDiscount, ProductDiscount } from "./promotion/discount";
+
+const pizzaDiscount = new ProductDiscount("002", 2, 33.38); 
+const TotalDiscount = new GlobalDiscount(30, 10);
+const promotional_rules = [pizzaDiscount, TotalDiscount];
+/**
+ * 
+ * @param orders an array of orders
+ * @returns Total price after applying discount
+ */
+
+function purchase(orders: IProduct[]): number {
+    const co = new Checkout(promotional_rules);
+    orders.map((item: IProduct) => {
+       co.scan(item)
+    })
+    let price  = co.total();
+    return price
+}
+ 
+console.log("Order: 001,002,003 =>  Total Price: " + purchase(orders__1));
+console.log("Order: 002,001,002 =>   Total Price: " + purchase(orders__2));
+console.log("Order: 002,001,002,003 =>   Total Price: " + purchase(orders__3));
+ 
